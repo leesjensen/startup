1d1914141a1891bb93764d7a72fb5815ec8bcc33
Lee <leesjensen@gmail.com>
2025-01-11 19:33:59 -0700

    impl(play) got sounds playing

--- src/play/play.jsx
+++ src/play/play.jsx
@@ -1,11 +1,60 @@
 import React from 'react';
 import './play.css';
 
+const calmSoundTypes = ['rain', 'thunder', 'waves', 'bowl', 'static', 'wind'];
+
+const calmSoundAudio = calmSoundTypes.reduce((acc, sound) => {
+  acc[sound] = new Audio(`/sounds/${sound}.mp3`);
+  acc[sound].loop = true;
+  return acc;
+}, {});
+
 export function Play() {
-  const calmMessages = ['Tal calmed by cars', 'Jordan calmed by gunshots', 'John calmed by rain', '민수 calmed by waves', 'Sai calmed by thunder'].reduce((a, calm, i) => {
-    a.push(<div key={i}>{calm}</div>);
-    return a;
+  const [calmMessages, setCalmMessages] = React.useState([]);
+  const [isPlaying, setIsPlaying] = React.useState(false);
+
+  React.useEffect(() => {
+    setCalmMessages(getCalmMessages());
+
+    return () => {
+      Object.values(calmSoundAudio).forEach((audio) => {
+        audio.pause();
+      });
+    };
   }, []);
+
+  function getCalmMessages() {
+    const mockedMessages = ['Bud calmed by static', 'Tal calmed by cars', 'Jordan calmed by gunshots', 'John calmed by rain', '민수 calmed by waves', 'Sai calmed by thunder'];
+    return mockedMessages.map((calm, i) => {
+      return <div key={i}>{calm}</div>;
+    });
+  }
+
+  function togglePlayAll() {
+    if (!isPlaying) {
+      const checkedSounds = calmSoundTypes.filter((sound) => document.getElementById(sound).checked);
+      checkedSounds.forEach((sound) => {
+        const audio = calmSoundAudio[sound];
+        audio.play();
+      });
+    } else {
+      Object.values(calmSoundAudio).forEach((audio) => {
+        audio.pause();
+      });
+    }
+
+    setIsPlaying(!isPlaying);
+  }
+
+  function togglePlay(audioElement) {
+    if (isPlaying) {
+      if (audioElement.checked) {
+        calmSoundAudio[audioElement.value].play();
+      } else {
+        calmSoundAudio[audioElement.value].pause();
+      }
+    }
+  }
 
   return (
     <main className='container-fluid'>
@@ -14,32 +63,22 @@ export function Play() {
         <div className='player-controls'>
           <fieldset>
             <div>
-              <div className='input-group'>
-                <div className='input-group-text'>
-                  <input className='form-check-input' type='checkbox' value='rain' id='rain' />
+              {calmSoundTypes.map((sound, index) => (
+                <div className='input-group' key={index}>
+                  <div className='input-group-text'>
+                    <input className='form-check-input' type='checkbox' value={sound} id={sound} onClick={(e) => togglePlay(e.target)} />
+                  </div>
+                  <input type='text' className={`form-control ${document.getElementById(sound)?.checked ? 'active' : ''}`} disabled value={sound} id={`${sound}-label`} />
                 </div>
-                <input type='text' className='form-control' disabled value='rain' id='rain-label' />
-              </div>
-              <div className='input-group'>
-                <div className='input-group-text'>
-                  <input className='form-check-input' type='checkbox' value='thunder' id='thunder' />
-                </div>
-                <input type='text' className='form-control' disabled value='thunder' id='thunder-label' />
-              </div>
-              <div className='input-group'>
-                <div className='input-group-text'>
-                  <input className='form-check-input' type='checkbox' value='waves' id='waves' />
-                </div>
-                <input type='text' className='form-control' disabled value='waves' id='waves-label' />
-              </div>
+              ))}
             </div>
           </fieldset>
           <div className='input-group play-button-container'>
             <span className='input-group-text' id='username'>
               <img className='play-button' src='logo.svg' />
             </span>
-            <button className='btn btn-primary' type='button' id='play'>
-              Play
+            <button className='btn btn-primary' type='button' id='play' onClick={togglePlayAll}>
+              {isPlaying ? '⏸️' : '▶️'}
             </button>
           </div>
         </div>
