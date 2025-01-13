ce59517cfeb2abe5b3a08c5366bd0bca03a830d3
Lee <leesjensen@gmail.com>
2025-01-13 16:42:16 -0700

    impl(login) tracking state

--- src/app.jsx
+++ src/app.jsx
@@ -2,59 +2,90 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
-import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, NavLink, Route, Routes, useNavigate } from 'react-router-dom';
 import { Login } from './login/login';
 import { Play } from './play/play';
 import { About } from './about/about';
 
 export default function App() {
-  return (
-    <BrowserRouter>
+  const [activeUser, setActiveUser] = React.useState(null);
+
+  function AppHeader({ activeUser }) {
+    return (
       <header>
-        <nav className='navbar navbar-expand  bg-body-tertiary'>
-          <div className='container-fluid'>
-            <a className='navbar-brand'>
-              <img src='logo.svg' className='logo d-inline-block align-top' alt='' />
+        <nav className="navbar navbar-expand bg-body-tertiary">
+          <div className="container-fluid">
+            <a className="navbar-brand">
+              <img src="logo.svg" className="logo d-inline-block align-top" alt="Calmer logo" />
               almer
             </a>
-            <div className='navbar-nav'>
-              <NavLink className='nav-link' to='play'>
-                Play
-              </NavLink>
-              <NavLink className='nav-link' to='about'>
+            <div className="navbar-nav">
+              {activeUser && (
+                <NavLink className="nav-link" to="play">
+                  Play
+                </NavLink>
+              )}
+              <NavLink className="nav-link" to="about">
                 About
-              </NavLink>{' '}
-              <NavLink className='nav-link' to=''>
-                Logout
-              </NavLink>{' '}
+              </NavLink>
+              {activeUser ? (
+                <NavLink className="nav-link" to="logout">
+                  Logout
+                </NavLink>
+              ) : (
+                <NavLink className="nav-link" to="">
+                  Login
+                </NavLink>
+              )}
             </div>
           </div>
         </nav>
       </header>
-      <Routes>
-        <Route path='/' element={<Login />} exact />
-        <Route path='/play' element={<Play />} />
-        <Route path='/about' element={<About />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
+    );
+  }
+
+  function AppFooter({ activeUser }) {
+    return (
       <footer>
-        <nav className='navbar sticky-bottom bg-body-tertiary'>
-          <div className='container-fluid'>
+        <nav className="navbar sticky-bottom bg-body-tertiary">
+          <div className="container-fluid">
             <span>
-              <img src='logo-github.svg' />{' '}
-              <a className='navbar-text' href='https://github.com/leesjensen/startup'>
+              <img src="logo-github.svg" alt="GitHub logo" />
+              <a className="navbar-text" href="https://github.com/leesjensen/startup">
                 GitHub
               </a>
-            </span>{' '}
-            <span className='navbar-text'>Lee S Jensen</span>
+            </span>
+            <div>calming {activeUser}</div>
+            <span className="navbar-text">Lee S Jensen</span>
           </div>
         </nav>
       </footer>
-      <script defer src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js'></script>
+    );
+  }
+
+  return (
+    <BrowserRouter>
+      <AppHeader activeUser={activeUser} />
+      <Routes>
+        <Route path="/" element={<Login setActiveUser={setActiveUser} />} />
+        <Route path="/logout" element={<Logout setActiveUser={setActiveUser} />} />
+        <Route path="/play" element={<Play />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <AppFooter activeUser={activeUser} />
     </BrowserRouter>
   );
 }
 
+function Logout({ setActiveUser }) {
+  const navigate = useNavigate();
+  React.useEffect(() => {
+    setActiveUser(null);
+    navigate('/');
+  }, []);
+}
+
 function NotFound() {
-  return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
+  return <main className="container bg-warning text-center">404: Return to sender. Address unknown.</main>;
 }
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,26 +1,45 @@
 import React from 'react';
 import './login.css';
 
-export function Login() {
+export function Login({ setActiveUser }) {
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  function register() {
+    setActiveUser(username);
+  }
+
+  function login(e) {
+    e.preventDefault();
+    setActiveUser(username);
+  }
+
   return (
-    <main className='container-fluid'>
+    <main className="container-fluid">
       <form>
         <fieldset>
-          <div className='input-group mb-3'>
-            <span className='input-group-text' id='username'>
-              @
-            </span>
-            <input type='text' className='form-control' placeholder='Username' id='username' autoComplete='true' />
+          <div className="input-group mb-3">
+            <span className="input-group-text">@</span>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="username"
+              id="username"
+              onChange={(e) => setUsername(e.target.value)}
+              autoComplete="true"
+            />
           </div>
-          <div className='input-group mb-3'>
-            <span className='input-group-text' id='password'>
-              🔒
-            </span>
-            <input type='text' className='form-control' placeholder='password' id='password' />
+          <div className="input-group mb-3">
+            <span className="input-group-text">🔒</span>
+            <input type="password" className="form-control" placeholder="password" id="password" onChange={(e) => setPassword(e.target.value)} />
           </div>
-          <div className='login-buttons'>
-            <button className='btn btn-primary'>Submit</button>
-            <button className='btn btn-secondary'>Register</button>
+          <div className="login-buttons">
+            <button className="btn btn-primary" onClick={login}>
+              Login
+            </button>
+            <button type="button" className="btn btn-secondary" onClick={register}>
+              Register
+            </button>
           </div>
         </fieldset>
       </form>
