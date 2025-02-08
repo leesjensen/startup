0a807eafd93345fed5abb43a743d48bd86ca9d27
Lee <leesjensen@gmail.com>
2025-02-08 08:14:51 -0700

    fix(ui) to be consistent

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function About() {
   return (
     <main className='container-fluid view-play'>
-      <h3>About Us</h3>
+      <h3>Calming welcome</h3>
       <p>
         In a world filled with loud, distracting, anxiety inducing voices, what we need is something to slow us down and get our minds into a stream of focused mindfulness. Open Calmer, select your
         favorite tones, and feel the peace. You can save your preferred calming environment, and also see who is calming down.
